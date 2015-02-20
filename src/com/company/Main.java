a7005382e0c32c828cefa0dc763fa2fa8ca53a30
Raphael <gemini88mill@gmail.com>
2015-02-19 23:29:56 -0500

    added secondary comments and javadoc to Main()

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -15,26 +15,29 @@ public class Main  {
      */
     public static void main(String[] args) {
 	// start
-        SimpleDateFormat sdf = new SimpleDateFormat("");
+        //SimpleDateFormat sdf = new SimpleDateFormat("");
         Account checking = new Account(100.00, new Date(), 319650141);
         Account savings = new Account(150.00, new Date(), 319650142);
 
         //open the bank
         bank = new Bank();
-        bank.start();
+
 
         //add money to accounts with timestamps for transactions.
         bank.setChecking(checking);
         bank.setSavings(savings);
 
-
+        //prints out current information regarding checking and savings account
         print();
+
         makeDeposit(17.07, "checking");
+        // --------------------------------------
         try {
             bank.sleep(3000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+        // sleeps program for 3 secs to test for timestamp issues
         makeWithdrawal(32.17, "savings");
 
         print();
